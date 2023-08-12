cfcc412b1e0238d63d925e787978d719bdba7680
EngelberAmaya <engelberamaya@gmail.com>
2023-08-12 13:58:06 -0400

    Consume the API through the custom hook

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,13 +1,27 @@
+import { useGetTodosQuery } from "./store/apis/todosApi"
 
 export const TodoApp = () => {
+
+    const { data: todos = [], isLoading} = useGetTodosQuery();
 
     return (
         <>
             <h1>Todos - RTK Query</h1>
             <hr />
-            <h4>isLoading...</h4>
+            <h4>isLoading: { isLoading ? 'True' : 'False' }</h4>
 
             <pre>...</pre>
+
+            <ul>
+                {
+                    todos.map( ({ title, id }) => (
+                        <li key={ id }>
+                            <strong>{ todos.completed ? 'Done' : 'Pending' } </strong>
+                            { title }
+                        </li>
+                    ))
+                }
+            </ul>
 
             <button>Next Todo</button>
         </>
